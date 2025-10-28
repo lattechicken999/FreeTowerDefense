969b5bbb674488eb3e94d6733a1120a4018cae0b
LSM <tmvkfmxk8962@gmail.com>
2025-10-28 11:55:38 +0900

    [feat] GameManager 메서드 추가

--- Assets/GamePlayManager/GameManager.cs
+++ Assets/GamePlayManager/GameManager.cs
@@ -3,45 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     private bool _isGameStart;
 
-    //GameManager.instance
-    static GameManager instance;
-    //싱글톤으로 작성해보기
-
-
     /// <summary>
-    /// 게임플레이 시작시 UI로 메서드호출
+    /// 씬넘버를 받으면 해당씬 호출
     /// </summary>
-    public void GameStartUI()
+    public void MoveScene(string sceneNum)
     {
-        //UI기능 호출
+        SceneManager.LoadScene(sceneNum);
     }
 
     /// <summary>
-    /// 게임플레이 시작시 UI로 메서드호출
+    /// 게임 시작 버튼을 누르면 스테이지씬 호출
     /// </summary>
-    public void GameStartStageManager()
+    public void GameStartButton()
     {
-        //UI기능 호출
+        SceneManager.LoadScene("Stage");
     }
 
-    //버튼이 눌렸을 때 수행할 기능을 제작하셔야 함, 여기가 이상민씨 작업의 끝
-    public void NextStage()
+    /// <summary>
+    /// 게임 종료 버튼을 누르면 게임종료씬 호출
+    /// </summary>
+    public void GameEndButton()
     {
-        Debug.Log("다음스테이지 이동");
-        SceneManager.LoadScene("인게임 씬");
+        SceneManager.LoadScene("GameEnd");
     }
-
-    public void setisGameover()
-    {
-        _isGameStart = true;
-    }
-
-    //1. 이름으로 찾기
-
-    //2. 태그로 찾기
-    //3. 드래그로 찾기
 }

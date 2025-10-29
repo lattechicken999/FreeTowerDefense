d101184f2f0ce7c2a2b36d990aad8c3c8ef96c06
mibana <ksy661004@gmail.com>
2025-10-29 14:11:49 +0900

    [feat] distance measurement
    배틀 매니저에 거리 측정 로직 추가

--- Assets/Scripts/BattleManager/BattleManager.cs
+++ Assets/Scripts/BattleManager/BattleManager.cs
@@ -4,25 +4,65 @@ using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    public List<GoldManager> units = new List<GoldManager>();
-    public List<GoldManager> monsters = new List<GoldManager>();
+    /// <summary>
+    /// 테스트용 리스트 나중에 변경----------
+    /// </summary>
+    public List<GameObject> TestObjects;
+    /// <summary>
+    /// 가장 가까운 오브젝트
+    /// </summary>
+    public GameObject _shortEnemy = null;
+    /// <summary>
+    /// 가장 짧은 거리 측정용
+    /// </summary>
+    public float _shortDistance;
 
+    /// <summary>
+    /// 테스트용 사거리 삭제 예정----------
+    /// </summary>
+    public float _attackRange = 3f;
 
+    /// <summary>
+    /// 사거리 내에 적을 공격하는 메서드
+    /// </summary>
     void Battle()
     {
 
-    }
+        // 테스트용 오브젝트 3개 생성-----------
+        TestObjects = new List<GameObject>();
+        GameObject object1 = new GameObject("Enemy1");
+        GameObject object2 = new GameObject("Enemy2");
+        GameObject object3 = new GameObject("Enemy3");
 
-    // 유닛이 공격할 몬스터 탐색
-    GoldManager FindTarget(GoldManager unit)
-    {
-        GoldManager selected = null;
-        return selected;
-    }
+        // 테스트용 오브젝트 위치 설정------------
+        object1.transform.position = new Vector3(0, 0, 0);
+        object2.transform.position = new Vector3(1, 0, 0);
+        object3.transform.position = new Vector3(2, 0, 0);
 
-    // 유닛이 몬스터 공격할때
-    void Damage(GoldManager attacker, GoldManager target)
-    {
+        // 테스트용 리스트에 추가-----------
+        TestObjects.Add(object1);
+        TestObjects.Add(object2);
+        TestObjects.Add(object3);
 
+        // 재사용시 문제를 없애기 위해 초기화
+        _shortDistance = 0;
+
+        // 모두 둘러보고 가장가까운 곳 찾기
+        foreach (GameObject obj in TestObjects)
+        {
+            float distance = Vector3.Distance(transform.position, obj.transform.position);
+
+            // 더 작은 값을 찾으면 갱신
+            if (distance < _shortDistance)
+            {
+                _shortDistance = distance;
+                _shortEnemy = obj;
+            }
+        }
+        // 사거리 보다 짧으면 
+        if (_attackRange < _shortDistance)
+        {
+            // 나중에 공격 로직 추가
+        }
     }
 }

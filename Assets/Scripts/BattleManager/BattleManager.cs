db4717220b6d4fda73882d6cd89ba97ef5f74d2e
mibana <ksy661004@gmail.com>
2025-10-30 10:51:39 +0900

    [feat] 몬스터 공격, 유닛 공격 추가
    유닛 공격시 타겟이 있는지 확인하는 작업 추가, 몬스터 공격 요청시 성벽 hp 반환

--- Assets/Scripts/BattleManager/BattleManager.cs
+++ Assets/Scripts/BattleManager/BattleManager.cs
@@ -22,10 +22,11 @@ public class BattleManager : MonoBehaviour
     /// </summary>
     public float _attackRange = 3f;
 
+
     /// <summary>
     /// 사거리 내에 가장 가까운 적을 찾는 메서드
     /// </summary>
-    public GameObject Trarget()
+    public GameObject Target()
     {
         // 테스트용 오브젝트 3개 생성-----------
         TestObjects = new List<GameObject>();
@@ -45,6 +46,7 @@ public class BattleManager : MonoBehaviour
 
         // 재사용시 문제를 없애기 위해 초기화
         _shortDistance = Mathf.Infinity;
+        _shortEnemy = null;
 
         // 모두 둘러보고 가장가까운 곳 찾기
         foreach (GameObject obj in TestObjects)
@@ -66,27 +68,51 @@ public class BattleManager : MonoBehaviour
         return null;
     }
 
-    public int UintAttack(string unitName, string monsterName)
+
+    /// <summary>
+    /// unit에서 공격을 요청했을때 사거리 내에 적이 있다면 데미지 계산
+    /// </summary>
+    /// <param name="unitAttack">유닛 공격력</param>
+    /// <returns></returns>
+    public int UnitAttack(int unitAttack)
     {
-
-
-        //GameObject target = Trarget();
-        //if (target != null)
-        //{
-        //    int damage = Damage(attackPower, defensePower);
-        //    return damage;
-        //}
-        return 0;
+        Monster monster = _shortEnemy.GetComponent<Monster>();
+        // 사거리 내에 적이 없을 때
+        if (_shortEnemy == null)
+        {
+            return 0;
+        }
+        // 지금 몬스터는 실수형, 유닛은 정수형이라 일단 정수형으로 바꾸고 나중에 상의후 변경 예정
+        int monsterDefense = (int)monster._DefensePoint;
+        int damage = Damage(unitAttack, monsterDefense);
+        
+        return damage;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="attack">유닛 공격력</param>
+    /// <param name="defense">몬스터 방어력</param>
+    /// <returns></returns>
     public int Damage(int attack, int defense)
     {
         int damage = attack - defense;
         return damage;
     }
 
-    public void MonsterAttack()
+    /// <summary>
+    /// 몬스터가 성벽을 공격할 때
+    /// </summary>
+    /// <param name="attack">몬스터 공격력</param>
+    /// <param name="defense">성벽 방어력</param>
+    /// <param name="hp">성벽 공격력</param>
+    /// <returns></returns>
+    public int MonsterAttack(int attack, int defense, int hp)
     {
+        hp -= attack - defense;
+        int wallHp = hp;
 
+        return wallHp;
     }
 }

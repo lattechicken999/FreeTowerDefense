089630a08dca2b475972fd534ce44b8b6b37432d
mibana <ksy661004@gmail.com>
2025-10-30 12:00:38 +0900

    [feat] 배틀 매니저 구독 추가
    MonsterManager에서 이벤트 발생 발생시 받아와서 리스트에 저장, 타겟 저장하는 기능 ||->&& 수정

--- Assets/Scripts/BattleManager/BattleManager.cs
+++ Assets/Scripts/BattleManager/BattleManager.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class BattleManager : MonoBehaviour
 {
     /// <summary>
-    /// 테스트용 리스트 나중에 변경----------
+    /// 몬스터 정보를 저장하는 리스트
     /// </summary>
-    public List<GameObject> TestObjects;
+    public List<GameObject> MonsterObjects;
     /// <summary>
     /// 가장 가까운 오브젝트
     /// </summary>
@@ -17,40 +17,82 @@ public class BattleManager : MonoBehaviour
     /// </summary>
     public float _shortDistance;
 
+    // 테스트용 사거리 삭제 예정
+    #region
+    //public float _attackRange = 3f;
+    #endregion
+
     /// <summary>
-    /// 테스트용 사거리 삭제 예정----------
+    /// 몬스터 매니저 구독
     /// </summary>
-    public float _attackRange = 3f;
+    private void OnEnable()
+    {
+        MonsterManager.Instance._notifiedMonsterMake += OnMonstersUpdated;
+    }
+
+    /// <summary>
+    ///  몬스터 매니저 구독 해제
+    /// </summary>
+    private void OnDisable()
+    {
+        MonsterManager.Instance._notifiedMonsterMake -= OnMonstersUpdated;
+    }
+
+    /// <summary>
+    /// 구독에서 몬스터 리스트를 받아와서 저장
+    /// </summary>
+    /// <param name="aliveMonsters"></param>
+    private void OnMonstersUpdated(List<Monster> addMonster)
+    {
+        MonsterObjects = new List<GameObject>();
+        foreach (var  monster in addMonster)
+        {
+            if (monster != null)
+            {
+                MonsterObjects.Add(monster.gameObject);
+            }
+        }
+    }
 
 
     /// <summary>
     /// 사거리 내에 가장 가까운 적을 찾는 메서드
     /// </summary>
-    public GameObject Target()
+    /// <param name="range">유닛 사거리</param>
+    /// <returns></returns>
+    public GameObject Target(float range)
     {
-        // 테스트용 오브젝트 3개 생성-----------
-        TestObjects = new List<GameObject>();
-        GameObject object1 = new GameObject("Enemy1");
-        GameObject object2 = new GameObject("Enemy2");
-        GameObject object3 = new GameObject("Enemy3");
+        // 테스트용 몬스터 리스트 삭제 예정 주석 처리 해둠
+        #region
+        //// 테스트용 오브젝트 3개 생성-----------
+        //MonsterObjects = new List<GameObject>();
+        //GameObject object1 = new GameObject("Enemy1");
+        //GameObject object2 = new GameObject("Enemy2");
+        //GameObject object3 = new GameObject("Enemy3");
 
-        // 테스트용 오브젝트 위치 설정------------
-        object1.transform.position = new Vector3(0, 0, 0);
-        object2.transform.position = new Vector3(1, 0, 0);
-        object3.transform.position = new Vector3(2, 0, 0);
+        //// 테스트용 오브젝트 위치 설정------------
+        //object1.transform.position = new Vector3(0, 0, 0);
+        //object2.transform.position = new Vector3(1, 0, 0);
+        //object3.transform.position = new Vector3(2, 0, 0);
 
-        // 테스트용 리스트에 추가-----------
-        TestObjects.Add(object1);
-        TestObjects.Add(object2);
-        TestObjects.Add(object3);
+        //// 테스트용 리스트에 추가-----------
+        //MonsterObjects.Add(object1);
+        //MonsterObjects.Add(object2);
+        //MonsterObjects.Add(object3);
+        #endregion
 
         // 재사용시 문제를 없애기 위해 초기화
         _shortDistance = Mathf.Infinity;
         _shortEnemy = null;
 
         // 모두 둘러보고 가장가까운 곳 찾기
-        foreach (GameObject obj in TestObjects)
+        foreach (GameObject obj in MonsterObjects)
         {
+            // 터지는거 방지용
+            if (obj == null)
+            {
+                continue;
+            }
             float distance = Vector3.Distance(transform.position, obj.transform.position);
 
             // 더 작은 값을 찾으면 갱신
@@ -61,7 +103,7 @@ public class BattleManager : MonoBehaviour
             }
         }
         // 사거리 보다 짧으면 
-        if (_shortEnemy != null || _attackRange <= _shortDistance)
+        if (_shortEnemy != null && range <= _shortDistance)
         {
             return _shortEnemy;
         }
@@ -76,12 +118,12 @@ public class BattleManager : MonoBehaviour
     /// <returns></returns>
     public int UnitAttack(int unitAttack)
     {
-        Monster monster = _shortEnemy.GetComponent<Monster>();
         // 사거리 내에 적이 없을 때
         if (_shortEnemy == null)
         {
             return 0;
         }
+        Monster monster = _shortEnemy.GetComponent<Monster>();
         // 지금 몬스터는 실수형, 유닛은 정수형이라 일단 정수형으로 바꾸고 나중에 상의후 변경 예정
         int monsterDefense = (int)monster._DefensePoint;
         int damage = Damage(unitAttack, monsterDefense);
